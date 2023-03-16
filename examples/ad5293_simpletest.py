57560e18b4bef777fdfd3c848eb672e9205f9203
Cedar Grove Maker Studios <jan@goolsbey.net>
2023-03-16 11:06:13 -0700

    initial repo load

--- examples/ad5293_simpletest.py
+++ examples/ad5293_simpletest.py
@@ -1,21 +1,20 @@
-# SPDX-FileCopyrightText: Copyright (c) 2022 JG for Cedar Grove Maker Studios
+# SPDX-FileCopyrightText: Copyright (c) 2023 JG for Cedar Grove Maker Studios
 #
 # SPDX-License-Identifier: Unlicense
 
 from time import sleep
 import board
 from analogio import AnalogIn
-import cedargrove_ad5245
+import cedargrove_ad5293
 
 # wire pin A to +3.3v, pin B to GND, and pin W to A0
 
-ad5245 = cedargrove_ad5245.AD5245(address=0x2C)
+ad5293 = cedargrove_ad5293.AD5293(select=board.D6)
 wiper_output = AnalogIn(board.A0)
 
 while True:
-
-    ad5245.wiper = 255
-    print("Wiper set to %d" % ad5245.wiper)
+    ad5293.wiper = 255
+    print("Wiper set to %d" % ad5293.wiper)
     voltage = wiper_output.value
     voltage *= 3.3
     voltage /= 65535
@@ -23,8 +22,8 @@ while True:
     print("")
     sleep(1.0)
 
-    ad5245.wiper = 0
-    print("Wiper set to %d" % ad5245.wiper)
+    ad5293.wiper = 0
+    print("Wiper set to %d" % ad5293.wiper)
     voltage = wiper_output.value
     voltage *= 3.3
     voltage /= 65535
@@ -32,8 +31,8 @@ while True:
     print("")
     sleep(1.0)
 
-    ad5245.wiper = 126
-    print("Wiper set to %d" % ad5245.wiper)
+    ad5293.wiper = 126
+    print("Wiper set to %d" % ad5293.wiper)
     voltage = wiper_output.value
     voltage *= 3.3
     voltage /= 65535
